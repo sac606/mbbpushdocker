2dc3376e0137edf22a7b9eded75d2aec23c210cc
Sachin <sachinpandit1729@gmail.com>
2025-08-14 04:41:35 +0000

    2nd commit

--- src/main/java/com/mbblab3/Mbblab3Application.java
+++ src/main/java/com/mbblab3/Mbblab3Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Mbblab3Application {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Sachin");
 		SpringApplication.run(Mbblab3Application.class, args);
 	}
 
